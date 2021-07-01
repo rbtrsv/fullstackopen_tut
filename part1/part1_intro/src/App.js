91c03f78c8aa4c77e6b5e89326a925d47a7158ee
Robert Radoslav <robert.radoslav@protonmail.ch>
2021-07-01 17:14:03 +0300

    Pt 0: Diagrams and images added

---
+++ part1/part1_intro/src/App.js
@@ -0,0 +1,65 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react'
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// const App = () => {
+//   const now = new Date()
+//   const a = 10
+//   const b = 20
+
+//   return (
+//     <div>
+//       <p>Hello world, it is {now.toString()}</p>
+//       <p>
+//         {a} plus {b} is {a + b}
+//       </p>
+//     </div>
+//   )
+// }
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+// A strong convention is the idea of a root component called App at the top of the component tree of the application.
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+
+export default App;
---
+++ part1/part1_intro/src/index.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// The command below renders the components of the React-component with the name App defined in App.js into the div-element, defined in the file public/index.html, having the id value 'root'.
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
