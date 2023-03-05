c24f6311f1b88438a85657b41a36d52d432141c8
Robert Radoslav <robert.radoslav@protonmail.ch>
2023-03-05 23:41:11 +0200

    2.19*: Data for countries, step2

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,19 +1,27 @@
+import { useState } from "react"
 import CountryInfo from "./CountryInfo"
 
 const Countries = ({ countriesShown }) => {
-	if (countriesShown.length === 1) {
-			return <CountryInfo c={countriesShown[0]} />
-	} else if (countriesShown.length <= 10) {
-			return (
-					<div>
-							{countriesShown.map((c) => (
-									<div key={c.name.official}>{c.name.common}</div>
-							))}
-					</div>
-			)
-	} else if (countriesShown.length > 10) {
-			return <div>Too many matches, specify another filter</div>
-	}
+  const [country, setCountry] = useState("")
+
+  if (countriesShown.length === 1) {
+      return <CountryInfo c={countriesShown[0]} />
+  } else if (countriesShown.length <= 10) {
+      return (
+          <div>
+              {countriesShown.map((c) => (
+
+                  <div key={c.name.official}>
+                    {c.name.common}
+                    <button onClick={() => setCountry(c)}>show</button>
+                  </div>
+              ))}
+              {country ? <CountryInfo c={country} /> : null}
+          </div>
+      )
+  } else if (countriesShown.length > 10) {
+      return <div>Too many matches, specify another filter</div>
+  }
 }
 
 export default Countries
