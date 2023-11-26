ff7f6d00b30d5eed4b5171f852619790fcbd5fb8
Robert Radoslav <robert.radoslav@protonmail.ch>
2023-11-26 21:29:11 +0200

    3.8*: Phonebook backend step8

--- part3/index.js
+++ part3/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const morgan = require('morgan');
 const app = express();
 
+morgan.token('body', (req) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
 app.use(express.json());
 app.use(morgan('tiny'));
 
