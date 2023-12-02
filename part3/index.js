b900a416a1be2db5a67dea833568b6e2d57ed11a
Robert Radoslav <robert.radoslav@protonmail.ch>
2023-12-02 13:12:02 +0200

    3.16: Phonebook database, step4

--- part3/index.js
+++ part3/index.js
@@ -13,20 +13,24 @@ app.use(express.static('build'))
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-app.get("/api/persons", (req, res) => {
-  Person.find({}).then(persons => {
-    res.json(persons);
-  });
+app.get("/api/persons", (req, res, next) => {
+  Person.find({})
+    .then(persons => {
+      res.json(persons);
+    })
+    .catch(error => next(error));
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  Person.findById(req.params.id).then(person => {
-    if (person) {
-      res.json(person);
-    } else {
-      res.status(404).send('Person not found');
-    }
-  });
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).send('Person not found');
+      }
+    })
+    .catch(error => next(error));
 });
 
 app.get("/info", (req, res) => {
@@ -39,16 +43,15 @@ app.get("/info", (req, res) => {
   });
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  // The exercise 3.15 was already done.
+app.delete("/api/persons/:id", (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
     .then(() => {
       res.status(204).end();
     })
-    .catch(error => res.status(400).send(error.message));
+    .catch(error => next(error));
 });
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   const { name, number } = req.body;
 
   if (!name || !number) {
@@ -61,8 +64,22 @@ app.post("/api/persons", (req, res) => {
     .then(savedPerson => {
       res.json(savedPerson);
     })
-    .catch(error => res.status(400).send(error.message));
+    .catch(error => next(error));
 });
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return res.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message });
+  } else {
+    return res.status(500).json({ error: 'internal server error' });
+  }
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
