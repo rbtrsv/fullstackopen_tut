79e3f6ea3e26b28e04826b100a91950d0254dc05
Robert Radoslav <robert.radoslav@protonmail.ch>
2023-12-03 22:48:30 +0200

    3.20*: Phonebook database, step8

--- part3/index.js
+++ part3/index.js
@@ -83,6 +83,13 @@ app.put("/api/persons/:id", (req, res, next) => {
     .catch(error => next(error));
 });
 
+app.use((error, req, res, next) => {
+  if (error.name === 'ValidationError') {
+    return res.status(400).send({ error: error.message });
+  }
+  next(error);
+});
+
 const errorHandler = (error, req, res, next) => {
   console.error(error.message);
 
