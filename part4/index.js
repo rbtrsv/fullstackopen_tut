2a03cb8055a1380705536cbddbb4232b966a5a64
Robert Radoslav <robert.radoslav@protonmail.ch>
2023-12-08 23:15:31 +0200

    4.1 Blog list, step1, pt.1

--- part4/index.js
+++ part4/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-require("dotenv").config()
+const config = require("./utils/config")
+const logger = require("./utils/logger")
+const middleware = require('./utils/middleware')
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -13,11 +15,12 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+
+app.use(middleware.requestLogger)
 
 app.get('/api/blogs', (request, response) => {
   Blog
@@ -37,7 +40,9 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
